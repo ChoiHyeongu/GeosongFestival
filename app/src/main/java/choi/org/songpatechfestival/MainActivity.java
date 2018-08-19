624fb755417e0b17469bea46c58d98e35fe07b49
ChoiHyeongu <a11dsd123@gmail.com>
2018-08-20 00:42:22 +0900

    Add fragment

--- app/src/main/java/choi/org/songpatechfestival/MainActivity.java
+++ app/src/main/java/choi/org/songpatechfestival/MainActivity.java
@@ -3,31 +3,42 @@ package choi.org.songpatechfestival;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    Fragment listFragment;
+    Fragment boothListFragment;
+    Fragment scheduleFragment;
+    Fragment informationFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listFragment = new ListFragment();
+        boothListFragment = new BoothListFragment();
+        scheduleFragment = new ScheduleListFragment();
+        informationFragment = new InformationFragment();
 
-        getSupportFragmentManager().beginTransaction().add(R.id.main_container, listFragment).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, scheduleFragment).commit();
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.main_bottomnavigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.action_home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, listFragment).commit();
+                    case R.id.action_schedule:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, scheduleFragment).commit();
+                        break;
+
+                    case R.id.action_boothList:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, boothListFragment).commit();
+                        break;
+
+                    case R.id.action_information:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, informationFragment).commit();
                         break;
                 }
                 return true;
