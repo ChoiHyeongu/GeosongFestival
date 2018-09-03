690f03081d9097f898dd5169b20ccd049172c57f
ChoiHyeongu <a11dsd123@gmail.com>
2018-09-04 08:29:03 +0900

    Add booth click event

--- app/src/main/java/choi/org/songpatechfestival/BoothListFragment.java
+++ app/src/main/java/choi/org/songpatechfestival/BoothListFragment.java
@@ -1,5 +1,6 @@
 package choi.org.songpatechfestival;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -24,11 +26,18 @@ public class BoothListFragment extends Fragment {
         boothList = (ListView) rootView.findViewById(R.id.boothFragment_boothList);
         boothItemArrayList = new ArrayList<BoothItem>();
 
-        for (int i = 1; i < 10; i++) {
-            boothItemArrayList.add(new BoothItem(R.mipmap.icon_home, "Booth Name" + i, "What to do"));
-        }
+        boothItemArrayList.add(new BoothItem(R.mipmap.icon_home, "Booth Name", "What to do"));
 
         boothList.setAdapter(new BoothListAdapter(getActivity(), boothItemArrayList));
+
+        boothList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                parent.getItemAtPosition(position);
+                Intent intent = new Intent(getActivity(), BoothActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
