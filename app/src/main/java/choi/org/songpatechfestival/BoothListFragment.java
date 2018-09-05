5210d5e2695647ef8cba6fef53f1896e4811e88a
ChoiHyeongu <a11dsd123@gmail.com>
2018-09-05 21:55:12 +0900

    Remove Booth detail

--- app/src/main/java/choi/org/songpatechfestival/BoothListFragment.java
+++ app/src/main/java/choi/org/songpatechfestival/BoothListFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,27 +18,23 @@ public class BoothListFragment extends Fragment {
 
     ListView boothList;
     ArrayList<BoothItem> boothItemArrayList;
+    ImageView boothImage;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_booth, container, false);
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_booth, container, false);
 
         boothList = (ListView) rootView.findViewById(R.id.boothFragment_boothList);
         boothItemArrayList = new ArrayList<BoothItem>();
 
-        boothItemArrayList.add(new BoothItem(R.mipmap.icon_home, "Booth Name", "What to do"));
+        boothItemArrayList.add(new BoothItem(R.mipmap.icon_schedule, "Booth Name1", "What to do"));
+        boothItemArrayList.add(new BoothItem(R.mipmap.icon_home, "Booth Name2", "What to do"));
+        boothItemArrayList.add(new BoothItem(R.mipmap.icon_home, "Booth Name3", "What to do"));
+        boothItemArrayList.add(new BoothItem(R.mipmap.icon_home, "Booth Name4", "What to do"));
+
 
         boothList.setAdapter(new BoothListAdapter(getActivity(), boothItemArrayList));
-
-        boothList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                parent.getItemAtPosition(position);
-                Intent intent = new Intent(getActivity(), BoothActivity.class);
-                startActivity(intent);
-            }
-        });
 
         return rootView;
     }
