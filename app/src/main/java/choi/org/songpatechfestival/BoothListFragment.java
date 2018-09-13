3667d143cb560446942787e04d8bccfe0dc55999
ChoiHyeongu <a11dsd123@gmail.com>
2018-09-13 23:44:11 +0900

    Uploads

--- app/src/main/java/choi/org/songpatechfestival/BoothListFragment.java
+++ app/src/main/java/choi/org/songpatechfestival/BoothListFragment.java
@@ -24,21 +24,23 @@ public class BoothListFragment extends Fragment {
 
         String[] boothName = {
                 "운동장을 누벼라", "물풍선으로 날리는 스트레스", "자유투 대회", "종이공예작품전시", "생활공예품 전시 및 판매", "송파 노래방", "순발력테스트", "항공/드론 비행 시뮬레이션", "페활량 시합",
-                "고무줄 총 사격", "탁구", "생리주기팔찌 및 대안생리대 제작", "활동사진 및 작품 전시", "나도 왕년에는", "디지털 액자 만들기", "3D 프린터 제품 전시 및 판매", "IT 기술 체험", "타투",
-                "핀버튼 만들기", "보드게임", "영화 관람 및 비평", "크로스워드완성, 청구기호보물찾기"};
+                "고무줄 총 사격", "탁구", "생리주기팔찌 및 대안생리대 제작", "디지털 액자 만들기", "3D 프린터 제품 전시 및 판매", "IT 기술 체험", "타투",
+                "핀버튼 만들기", "보드게임", "영화 관람 및 비평", "크로스워드완성"
+        };
 
         String[] circleName = {
-                "202학급", "304학급", "농구", "종이공예", "생활공예창업", "보컬", "실내클라이밍", "스마트항공", "배드민턴", "학생회", "탁구", "배드민턴", "해양소년단", "방과후창체부", "창업연구",
+                "202학급", "304학급", "농구", "종이공예", "생활공예창업", "보컬", "실내클라이밍", "스마트항공", "배드민턴", "학생회", "탁구", "배드민턴", "창업연구",
                 "3D제품발명반", "알고리즘 연구", "보드게임", "디자인 기능", "보드게임", "영화감상", "도서"
         };
 
         String[] place = {
-                "운동장", "운동장", "농구장", "위클래스", "위클래스", "음악실", "103교실", "104교실", "105교실", "106교실", "108교실", "보건교육실", "9반 복도",
-                "거송관 복도", "물리실", "물리실", "306교실", "307교실", "308교실", "수학전용실", "국어전용실", "도서관"
+                "운동장", "운동장", "농구장", "위클래스", "위클래스", "음악실", "103교실", "104교실", "105교실", "106교실", "108교실", "보건교육실", "물리실", "물리실",
+                "306교실", "307교실", "308교실", "수학전용실", "국어전용실", "도서관"
         };
 
-        int[] boothImage={
-                R.mipmap.algo,R.mipmap.iot, R.mipmap.ads, R.mipmap.baking, R.mipmap.drone, R.mipmap.pingpong, R.mipmap.startup, R.mipmap.vocal
+        int[] boothImage = {
+                R.mipmap.playground, R.mipmap.playground, R.mipmap.playground, R.mipmap.paper, R.mipmap.lifeitem, R.mipmap.sing, R.mipmap.test, R.mipmap.drone, R.mipmap.competition, R.mipmap.gun, R.mipmap.tabletennis,
+                R.mipmap.medical, R.mipmap.printer, R.mipmap.printer, R.mipmap.it, R.mipmap.tatto, R.mipmap.pinbutton, R.mipmap.boardgame, R.mipmap.movie, R.mipmap.crossword
         };
 
         boothList = (ListView) rootView.findViewById(R.id.boothFragment_boothList);
--- app/src/main/java/choi/org/songpatechfestival/ScheduleListFragment.java
+++ app/src/main/java/choi/org/songpatechfestival/ScheduleListFragment.java
@@ -24,11 +24,13 @@ public class ScheduleListFragment extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_schedule, container, false);
 
         String[] event = {
-            "스마트 항공반", "가야금반", "김규리", "차동엽 외 7명", "김동환 외 5명", "방송반", "부스활동", "스마트항공반", "어머니 10명", "구휘 외 5명"
+                "스마트 항공반", "가야금반", "차동엽 외 7명", "김동환 외 5명", "방송반", "부스활동", "스마트항공반", "어머니 10명", "구휘 외 5명", "댄스 선생님", "이현준",
+                "강석진", "이경민, 최준영", "문태우, 이희명", "이정훈", "김동근, 김재현", "임범수"
         };
 
         String[] detail = {
-            "드론 시연", "가야금 합주", "25현 가야금 독주", "두드림 난타", "태권도 퍼포먼스", "동아리 활동 영상", "부스 활동", "드론시연", "어머니 가야금 합주", "댄스"
+                "드론 시연", "가야금 합주", "두드림 난타", "태권도 퍼포먼스", "동아리 활동 영상", "부스 활동", "드론시연", "어머니 가야금 합주", "댄스", "댄스 선수생", "Hello",
+                "Puzzle", "이 별", "L4L", "모든 날 모든 순간", "북", "이등병의 편지"
         };
 
         scheduleList = (ListView) rootView.findViewById(R.id.scheduleFragment_scheduleList);
